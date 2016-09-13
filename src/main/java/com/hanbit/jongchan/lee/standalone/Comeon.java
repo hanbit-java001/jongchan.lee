660f3a0e08ebb2536a8c6cb8d8e4a6e9aba4b454
jafcxg <jafcxg@hb5014>
2016-09-13 12:00:19 +0900

    컴온삭제

---
+++ src/main/java/com/hanbit/jongchan/lee/standalone/Comeon.java
@@ -0,0 +1,9 @@
+package com.hanbit.jongchan.lee.standalone;
+
+public class Comeon {
+public static void main(String[] args) {
+	System.out.println("안녕하세요");
+	System.err.println("굿바이");
+	System.out.println("바이바이바이");
+}
+}
