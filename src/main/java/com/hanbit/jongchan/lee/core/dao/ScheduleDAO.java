2a75371c2dab64b9fa5c70a286db38c71d37aa1d
jafcxg <jafcxg@hb5014>
2016-09-13 14:39:42 +0900

    게시판 상세보기

--- src/main/java/com/hanbit/jongchan/lee/core/dao/ScheduleDAO.java
+++ src/main/java/com/hanbit/jongchan/lee/core/dao/ScheduleDAO.java
@@ -8,6 +8,15 @@ import java.util.List;
 
 import com.hanbit.jongchan.lee.core.vo.ScheduleVO;
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class ScheduleDAO {
 
 	private Connection getConnection() {
@@ -97,19 +106,68 @@ public class ScheduleDAO {
 		return result;
 	}
 
-		public int deleteSchedule(String schedule) {
-			Connection connection = getConnection();
+	public int deleteSchedule(String scheduleId) {
+		Connection connection = getConnection();
 
-			String sql = "DELETE FROM SCHEDULE WHERE SCHEDULE_ID = ?";
+		String sql = "DELETE FROM SCHEDULE WHERE SCHEDULE_ID = ?";
 
-			List params = new ArrayList();
-			params.add(schedule);
+		List params = new ArrayList();
+		params.add(scheduleId);
 
-			int result = executeSql(connection, sql, params);
+		int result = executeSql(connection, sql, params);
 
-			closeConnection(connection);
+		closeConnection(connection);
 
-			return result;
+		return result;
 	}
+
+	public List<ScheduleVO> selectSchedules(String startDt, String endDt) {
+		Connection connection = getConnection();
+
+		String sql = "SELECT SCHEDULE_ID, TITLE, MEMO, "
+				+ "START_DT, END_DT FROM SCHEDULE "
+				+ "WHERE START_DT <= ? AND END_DT >= ?";
+
+		List params = new ArrayList();
+		params.add(endDt);
+		params.add(startDt);
+
+		List<ScheduleVO> result = new ArrayList<ScheduleVO>();
+
+		try {
+			PreparedStatement statement = connection.prepareStatement(sql);
+
+			for (int i=0;i<params.size();i++) {
+				statement.setObject(i + 1, params.get(i));
+			}
+
+			ResultSet resultSet = statement.executeQuery();
+
+			while (resultSet.next()) {
+				ScheduleVO schedule = new ScheduleVO();
+
+				schedule.setScheduleId(resultSet.getString("SCHEDULE_ID"));
+				schedule.setTitle(resultSet.getString("TITLE"));
+				schedule.setMemo(resultSet.getString("MEMO"));
+				schedule.setStartDt(resultSet.getString("START_DT"));
+				schedule.setEndDt(resultSet.getString("END_DT"));
+
+				result.add(schedule);
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		closeConnection(connection);
+
+		return result;
+	}
+
+	public ScheduleVO selectSchedule(String scheduleId) {
+
+		return null;
+	}
+
 
 }
