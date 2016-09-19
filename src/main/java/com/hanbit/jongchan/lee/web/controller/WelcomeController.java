8d0d73abfd297d4ecbc44c9a064c0309697530d5
jafcxg <jafcxg@hb5014>
2016-09-19 17:21:58 +0900

    2016-09-19오후

---
+++ src/main/java/com/hanbit/jongchan/lee/web/controller/WelcomeController.java
@@ -0,0 +1,33 @@
+package com.hanbit.jongchan.lee.web.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class WelcomeController {
+
+	@RequestMapping("/")
+	public String welcome(Model model) {
+
+		model.addAttribute("name", "Hanbit");
+
+		return "welcome";
+	}
+
+	@RequestMapping("/api/data")
+	@ResponseBody
+	public Map getData() {
+		Map map = new HashMap();
+
+		map.put("name", "Hanbit");
+		map.put("message", "Hello");
+
+		return map;
+	}
+
+}
